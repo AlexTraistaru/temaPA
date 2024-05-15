24fdad66da0497f6d9898d40ac405ea6ea39ae21
AlexTraistaru <162322229+AlexTraistaru@users.noreply.github.com>
2024-05-15 17:13:04 +0300

    Add files via upload
    Adaugare cerinta2

--- bib1.c
+++ bib1.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "bib1.h"
 
-
+/// functii cerinta 1
 void eroare_file()
 {
     printf("fisierul nu a putut fi deschis!");
@@ -36,20 +34,19 @@ void adaug_inceput(team** head, char* team_name, int teammates, float total_poin
 
 void afisare(team* head)
 {
-    printf("lista este:\n");
     while (head != NULL)
     {
         printf("%d", head -> teammates);
-        printf("%s\n", head -> team_name);
+        printf("%s", head -> team_name);
         player* curent = &(head -> val);
         while (curent != NULL)
         {
-            printf("%s %s %d\n", curent -> firstName, curent -> secondName, curent -> points);
+            printf("%s %s %d", curent -> firstName, curent -> secondName, curent -> points);
             curent = curent -> next;
         }
-        printf("\n");
         head = head -> next;
     }
+    printf("\n");
 }
 
 
@@ -100,4 +97,92 @@ void cerinta1(FILE* file, team** lista, int nr_echipe)
     }
     for (i = nr_echipe - 1; i >= 0; i--)
         fprintf(file, "%s\n", lista[i] -> team_name);
+}
+
+
+/// functii cerinta 2
+void stergere_echipa_1(team** lista, int* nr_echipe)
+{
+    int i;
+    int gasit = 0;
+    int poz = -1; ///poz este indexul echipei pe care vreau sa o sterg
+    for (i = 0; i < *nr_echipe; i++)
+        if (lista[i] == punctajul_mic(lista, *nr_echipe))
+        {
+            poz = i;
+            gasit = 1;
+            break; ///ne oprim din cautare pentru ca am gasit deja o echipa si o sa o stergem
+        }
+    if (gasit == 1)
+    {
+        if (poz >= 0)
+            lista[poz - 1] -> next = lista[poz] -> next; ///refac legaturile intre componente
+        free(lista[poz] -> team_name);
+        free(lista[poz]);
+        for (i = poz; i < (*nr_echipe - 1); i++)
+            lista[i] = lista[i + 1];
+        (*nr_echipe)--;
+    }
+}
+
+
+void stergere_echipa_2(team** lista, int* nr_echipe)
+{
+    while (*nr_echipe > putere_2(*nr_echipe))
+    {
+        team* echipa_de_sters = punctajul_mic(lista, *nr_echipe);
+        stergere_echipa_1(lista, nr_echipe);
+    }
+}
+
+team* punctajul_mic(team** lista, int nr_echipe)
+{
+    int i;
+    int poz = 0;
+    float minim = lista[0] -> total_points;
+    for(i = 1; i < nr_echipe; i++)
+        if(lista[i] -> total_points <= minim)
+        {
+            minim = lista[i] -> total_points;
+            poz = i;
+        }
+    return lista[poz];
+}
+
+
+void medie(team** lista, int nr_echipe)
+{
+    int i;
+    for(i = 0; i < nr_echipe; i++)
+    {
+        team* curent = lista[i];
+        float suma = 0;
+        while(curent != NULL)
+        {
+            suma = suma + curent -> val.points;
+            curent = curent->next;
+        }
+        lista[i] -> total_points = (float)(suma / lista[i] -> teammates);
+    }
+}
+
+
+int putere_2(int nr_echipe)
+{
+    //printf("nr echipe=%d\n", nr_echipe);
+    int p = 1;
+    while (p * 2 <= nr_echipe)
+         p = p * 2;
+    //printf("p=%d", p);
+    return p;
+}
+
+
+void cerinta2(FILE* file, team** lista, int *nr_echipe)
+{
+    int i;
+    medie(lista, *nr_echipe);
+    stergere_echipa_2(lista, nr_echipe);
+    for(i = *nr_echipe - 1; i >= 0; i--)
+        fprintf(file, "%s", lista[i] -> team_name);
 }
--- main.c
+++ main.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "bib1.h"
-
 
 int main(int argc, char *argv[])
 {
-    FILE *file1 = fopen(argv[2], "rt");
+    int i;
+    FILE* file1 = fopen(argv[2], "rt");
     if (file1 == NULL)
         eroare_file();
 
 
-    FILE *file2 = fopen(argv[3], "wt");
+    FILE* file2 = fopen(argv[3], "wt");
     if (file2 == NULL)
         eroare_file();
+
+    FILE* file3 = fopen(argv[1], "rt");
+    if (file3 == NULL)
+        eroare_file();
+
+    int cerinte[5];
+    for (i = 0; i < 5; i++) ///i < 5 pentru ca avem 5 cerinte de facut
+        fscanf(file3, "%d", &cerinte[i]);
 
 
     int nr_echipe;
     team** lista = creare_lista(file1, &nr_echipe);
 
 
-    cerinta1(file2, lista, nr_echipe);
+    if (cerinte[0] == 1 && cerinte[1] == 0)
+        cerinta1(file2, lista, nr_echipe);
+
+
+    if (cerinte[1] == 1)
+        cerinta2(file2, lista, &nr_echipe);
 
 
     fclose(file1);
     fclose(file2);
-    int i;
+    fclose(file3);
+
+
     //printf("citirea s a facut cu succes! nr_echipe=%d", nr_echipe);
     for (i = 0; i < nr_echipe; i++)
         free(lista[i]);
